b2ac8716bda7b8050faa994b14e6d83726ec649b
Marco Ferrati <marco.ferrati@gmail.com>
2021-04-30 12:12:11 +0200

    Add tests for the example files

--- src/test/java/it/azzalinferrati/AppTest.java
+++ src/test/java/it/azzalinferrati/AppTest.java
@@ -1,20 +1,45 @@
 package it.azzalinferrati;
 
-import static org.junit.Assert.assertTrue;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class AppTest {
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        String[] examplesFile = (new File("examples/")).list();
+
+        Collection<Object[]> params = new ArrayList<>(100);
+        assert examplesFile != null;
+        for(String fileName : examplesFile) {
+            params.add(new Object[] { fileName });
+        }
+        return params;
+    }
+
+    String exampleFile;
+
+    public AppTest(String exampleFile) {
+        this.exampleFile = exampleFile;
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void testExample() throws Exception {
+        int systemExit;
+        try {
+            systemExit = catchSystemExit(() -> App.main(new String[]{"examples/" + exampleFile}));
+        } catch (AssertionError error) {
+            systemExit = 0;
+        }
+
+        assertEquals(0, systemExit);
     }
 }
